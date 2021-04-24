5c4139bb4633b58828fe86bad70b1c616ff1f7ce
Mustafa Emre <mustafaemre456@gmail.com>
2021-04-24 16:55:29 +0300

    meoApi

--- MakasAPI/Data/Repositories/Concrete/CustomerRepository.cs
+++ MakasAPI/Data/Repositories/Concrete/CustomerRepository.cs
@@ -69,5 +69,116 @@ namespace MakasAPI.Data.Repositories.Concrete
             return null;
 
         }
+
+        public List<Worker> GetWorkersBySaloon(int saloonId)
+        {
+            var workers = _context.Workers.Where(s => s.SaloonId == saloonId).ToList();
+            if (workers != null)
+            {
+                workers.OrderBy(s => s.WorkerRate);//worker rate ine göre listview e sıralanmış gönderim.
+                return workers;
+            }
+            return null;
+        }
+
+        public async Task<Appointment> AddAppointment(int customerId, int saloonId, int workerId, DateTime dateT)
+        {
+            if (customerId != 0 && saloonId != 0 && workerId != 0)
+            {
+                var appointmentToCreate = new Appointment
+                {
+                    CustomerId = customerId,
+                    SaloonId = saloonId,
+                    WorkerId = workerId,
+                    Date = dateT
+
+                };
+                _context.Add(appointmentToCreate);
+                await _context.SaveChangesAsync();
+                return appointmentToCreate;
+            }
+            return null;
+        }
+
+        public List<Appointment> GetAppointmentsById(int customerId)
+        {
+            var appointments = _context.Appointments.Where(s => s.CustomerId == customerId).ToList();
+            if (appointments != null)
+            {
+                appointments.Sort((x, y) => y.Date.CompareTo(x.Date));  //tarihi tersten sıralama fonksiyonu.
+                return appointments;
+            }
+            return null;
+        }
+
+        public async Task<Review> AddReview(int customerId, int saloonId, int workerId, int appointmentId, double rate, string comment)
+        {
+            if (rate != 0 && comment != null)
+            {
+                var reviewToCreate = new Review
+                {
+                    CustomerId = customerId,
+                    SaloonId = saloonId,
+                    WorkerId = workerId,
+                    AppointmentId = appointmentId,
+                    Rate = rate,
+                    Comment = comment
+
+                };
+                _context.Add(reviewToCreate);
+                await _context.SaveChangesAsync();
+                return reviewToCreate;
+            }
+            return null;
+        }
+
+        public List<Review> GetReviewsBySaloon(int saloonId)
+        {
+            var reviews = _context.Reviews.Where(s => s.SaloonId == saloonId).ToList();
+            if (reviews != null)
+            {
+                reviews.Reverse();
+                return reviews;
+            }
+            return null;
+        }
+
+        public async Task<Favorite> AddFavorite(int customerId, int saloonId)
+        {
+
+            var favoriteToCreate = new Favorite
+            {
+                CustomerId = customerId,
+                SaloonId = saloonId
+
+            };
+            _context.Add(favoriteToCreate);
+            await _context.SaveChangesAsync();
+            return favoriteToCreate;
+
+        }
+
+        public async Task<Favorite> UnFavorite(int id)
+        {
+            var favorite = _context.Favorites.FirstOrDefault(w => w.Id == id);
+            if (favorite != null)
+            {
+                _context.Remove(favorite);
+                await _context.SaveChangesAsync();
+                return favorite;
+            }
+            return null;
+        }
+
+        public List<Favorite> GetFavoritesByCustomer(int customerId)
+        {
+            var favorites = _context.Favorites.Where(s => s.CustomerId == customerId).ToList();
+            if (favorites != null)
+            {
+                favorites.Reverse();  //En son eklenen başta duracak şekilde.
+                return favorites;
+            }
+            return null;
+        }
     }
 }
