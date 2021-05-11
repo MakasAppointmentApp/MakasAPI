c548771a81c6fbff99c336fcb58ae065a8db734e
Mustafa Emre <mustafaemre456@gmail.com>
2021-05-11 06:05:57 +0300

    Favorites Image converted, favorites guided to Saloon Profile.

--- MakasAPI/Dtos/DtosForUsers/FavoriteByCustomerDto.cs
+++ MakasAPI/Dtos/DtosForUsers/FavoriteByCustomerDto.cs
@@ -8,6 +8,7 @@ namespace MakasAPI.Dtos.DtosForSaloon
     public class FavoriteByCustomerDto
     {
         public int Id { get; set; }
+        public int SaloonId { get; set; }
         public string SaloonName { get; set; }
         public byte[] SaloonImage { get; set; }
         public double SaloonRate { get; set; }
