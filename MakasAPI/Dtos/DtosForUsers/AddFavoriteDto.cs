5a26127c3aae519a9fbc13980bfd0fae4dcc1607
Mustafa Emre <mustafaemre456@gmail.com>
2021-05-12 07:02:29 +0300

    Favori sayfasında favori silindi. Salon profl sayfasında isFavorited kontrolü yapılı favorili değilse favorilenebildi ve favorili ise favoriden çıkartılabildi. tüm denemeler yapıldı Favorites tamamlandı.

---
+++ MakasAPI/Dtos/DtosForUsers/AddFavoriteDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MakasAPI.Dtos.DtosForUsers
+{
+    public class AddFavoriteDto
+    {
+        public int CustomerId { get; set; }
+        public int SaloonId { get; set; }
+    }
+}
